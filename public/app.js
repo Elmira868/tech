8d428daa9ae080073f7eac36fbe4b25277f8356b
elmira amjadian <elmiraamjadian5@gamil.com>
2025-01-29 19:36:20 +0330

    Creat menu and navbar

--- public/app.js
+++ public/app.js
@@ -3,13 +3,14 @@ const menuBtn = document.querySelector('.menu-btn');
 const sideMenu = document.querySelector('.side-menu');
 const closeMenuBtn = document.querySelector('.close-menu__btn');
 
+// Show menu function
 menuBtn.addEventListener('click', () => {
-    sideMenu.style.right = '0'; // منو نمایش داده شود
+    sideMenu.style.right = '0'; 
     sideMenu.style.display = 'block';
 });
-
+// Close menu function
 closeMenuBtn.addEventListener('click', () => {
-    sideMenu.style.right = '-100%'; // منو پنهان شود
+    sideMenu.style.right = '-100%'; 
 });
 
 
